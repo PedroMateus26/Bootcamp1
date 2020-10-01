1215a4879ac6ed707fcf6aded4c15bdca24d4932
PedroMateus26 <pedromateusneto@gmail.com>
2020-10-01 11:30:36 -0300

    Product repository, service, resource, GET ok

--- backend/src/main/java/com/pedromateus/dscatalog/dto/CategoryDTO.java
+++ backend/src/main/java/com/pedromateus/dscatalog/dto/CategoryDTO.java
@@ -1,8 +1,12 @@
 package com.pedromateus.dscatalog.dto;
 
+import java.io.Serializable;
+
 import com.pedromateus.dscatalog.entities.Category;
 
-public class CategoryDTO {
+public class CategoryDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 
 	private Long id;
 	private String name;
