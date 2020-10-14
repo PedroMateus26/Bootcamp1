1fe7a7f570f20ce6f8863a777ebdb250ec957a22
PedroMateus26 <pedromateusneto@gmail.com>
2020-10-14 08:27:17 -0300

    Listing with backend

--- front-web/src/pages/Catalog/components/ProductsDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductsDetails/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { ReactComponent as ArrowIcon } from '../../../../core/assets/images/arrow.svg'
-import { ReactComponent as ProductImage } from '../../../../core/assets/images/product.svg'
 import ProductPrice from '../../../../core/components/ProductPrice';
+import { Product } from '../../../../core/types/Product';
+import { makeRequest } from '../../../../core/utils/request';
 import './styles.scss'
 
 type ParamsType = {
@@ -11,7 +12,12 @@ type ParamsType = {
 
 const ProductDetails = () => {
     const { productId } = useParams<ParamsType>();
-    console.log(productId)
+    const [product,setProduct]=useState<Product>()
+    useEffect(()=>{
+       makeRequest({url:`/products/${productId}`})
+       .then(response=>setProduct(response.data))
+
+    },[productId]);
     return (
         <div className="producs-details-container">
             <div className="card-base border-radius-20 producs-details">
@@ -22,21 +28,19 @@ const ProductDetails = () => {
                 <div className="row product-details-card">
                     <div className="col-6 pr-5">
                         <div className="product-details-card text-center">
-                            <ProductImage className="product-details-image" />
+                            <img src={product?.imgUrl} alt={product?.name} className="product-details-image" />
                         </div>
                         <h1 className="product-details-name">
-                            Computador Desktop - Intel Core i7
+                           {product?.name}
                         </h1>
-                        <ProductPrice price="2.779,00"/>
+                        {product?.price && <ProductPrice price={product?.price}/>}
                     </div>
                     <div className="col-6 product-details-card">
                         <h1 className="product-description-title">
                             Decrição do Produto
                         </h1>
                         <p className="product-description-text">
-                            Projetado para garantir a produtividade no seu dia a dia
-                            O desempenho que você precisa para uma jornada eficiente
-                            é garantido pelos processadores Intel da família Core.
+                            {product?.description}
                         </p>
                     </div>
                 </div>
