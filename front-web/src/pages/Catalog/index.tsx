8ec1c1fb860860f718c9d87070d8b862b47e56ad
PedroMateus26 <pedromateusneto@gmail.com>
2020-10-14 10:38:30 -0300

    Finished catalog loaders

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import './styles.scss';
 import ProductCard from './components/ProductCard'
+import ProductCardLoader from './components/Loader/ProductCardLoader'
 import { Link } from 'react-router-dom';
-import { makeRequest } from '../../core/utils/request';
+import { makeRequest } from 'core/utils/request';
 import { url } from 'inspector';
-import { ProductResponse } from '../../core/types/Product';
+import { ProductResponse } from 'core/types/Product';
 
 
 
@@ -13,24 +14,30 @@ const Catalog=()=>{
     //Popular um estado no componente e listar os produtos dinâmincamente
     const [productResponse, setProductResponse]=useState<ProductResponse>()
     console.log(productResponse);
-    
+    const[isLoading, setIsLoading]=useState(false);
 
-    //Quando o componente iniciar buscar a lista de produtos
+    
     useEffect(()=>{
 
         const params={
             page:0,
             linesPerPage:12
         };
-        makeRequest({url:"/products",params}).then(response=>setProductResponse(response.data))
+        setIsLoading(true)
+        makeRequest({url:"/products",params})
+        .then(response=>setProductResponse(response.data))
+        .finally(()=>{
+            setIsLoading(false)
+        })
     },[])
 
     return (
     <div className="catalog-container">
         <h1 className="catalog-title">Catálogo de produtos</h1>
         <div className="catalog-products product-card">
-            {productResponse?.content.map(product=>(
-                <Link to={`/products/${product.id}`} key={product.id}><ProductCard product={product}/></Link>
+           {isLoading?<ProductCardLoader/>:
+            productResponse?.content.map(product=>(
+            <Link to={`/products/${product.id}`} key={product.id}><ProductCard product={product}/></Link>
             ))}
            
         </div>
