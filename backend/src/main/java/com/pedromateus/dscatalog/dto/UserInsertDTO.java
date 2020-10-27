a8d4a884a113ca2204704a57045081ab6870e6e8
PedroMateus26 <pedromateusneto@gmail.com>
2020-10-27 08:16:42 -0300

    custom constraintValidator

--- backend/src/main/java/com/pedromateus/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/pedromateus/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.pedromateus.dscatalog.dto;
 
+import com.pedromateus.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 
 	private static final long serialVersionUID = 1L;
